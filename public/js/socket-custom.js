594e4cdcda563ed3f7fb03a5aa090565b5b3e396
ElMariachi <zacag1500@gmail.com>
2020-08-27 19:17:59 -0500

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,23 @@
+//escuchar sucesos
+var socket = io();
+socket.on("connect", function () {
+    console.log("Conectado al servidor");
+});
+socket.on("disconnect", function () {
+    console.log("Perdimos conexión con el servidor");
+});
+//Enviar información
+socket.emit(
+    "enviarMensaje",
+    {
+        usuario: "Javier",
+        mensaje: "Hola Mundo",
+    },
+    function (resp) {
+        console.log("Respuesta server: ", resp);
+    }
+);
+//Escuchar información
+socket.on("enviarMensaje", function (mensaje) {
+    console.log("Servidor: ", mensaje);
+});
